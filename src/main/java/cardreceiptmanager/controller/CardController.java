0b53f15e9d608f8862b63d5b8e5298099fb4e7ef
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-01-29 13:52:13 +0200

    Card Controller in progress

--- src/main/java/cardreceiptmanager/controller/CardController.java
+++ src/main/java/cardreceiptmanager/controller/CardController.java
@@ -5,9 +5,9 @@ import cardreceiptmanager.model.entity.Card;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class CardController {
@@ -15,16 +15,54 @@ public class CardController {
     @Autowired
     private CardRepository cardRepository;
 
-    @RequestMapping(value = "/cards", method = RequestMethod.GET)
-    public String index(Model model) {
-
-        model.addAttribute("title", "Card Manager Test");
-        model.addAttribute("msg", "Hello, It is Card Manager");
-
+    // TODO: add permission for USER
+    @RequestMapping(value = "/card", method = RequestMethod.GET)
+     public String getAllCards(Model model) {
+        model.addAttribute("cards", cardRepository.findAll());
         return "cards";
     }
 
+    // TODO: add permission for USER
+    @RequestMapping("card/new")
+    public String add(Model model){
+        model.addAttribute("card", new Card());
+        return "cardForm";
+    }
 
+    // TODO: add permission for USER
+    @RequestMapping(value = "/card", method = RequestMethod.POST)
+    public String addCard(Card card) {
+        cardRepository.save(card);
+        return "redirect:/card/" + card.getId();
+    }
+
+    // TODO: add permission for USER
+    @RequestMapping(value = "/card/{id}", method = RequestMethod.GET)
+    public String getCard(@PathVariable Integer id, Model model) {
+        model.addAttribute("card", cardRepository.findOne(id));
+        return "card";
+    }
+
+    // TODO: add permission for ADMIN
+    @RequestMapping("/card/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        model.addAttribute("card", cardRepository.findOne(id));
+        return "cardForm";
+    }
+
+    // TODO: add permission for ADMIN
+    @RequestMapping(value = "/card", method = RequestMethod.PUT)
+    public String updateCard(Card card) {
+        cardRepository.save(card);
+        return "redirect:/card/" + card.getId();
+    }
+
+    // TODO: add permission for ADMIN
+    @RequestMapping(value = "/card/$id", method = RequestMethod.DELETE)
+    public String deleteCard(@PathVariable Integer id) {
+        cardRepository.delete(id);
+        return "redirect:/card";
+    }
 
     // TODO: delete it, only for test
     @RequestMapping("/create")
