854a757126acd513e61b50b2fb491ea0aac14880
Liubchenko <Elena.Liubchenko@vistajet.com>
2016-01-29 11:00:10 +0200

    added create test card method to controller

--- src/main/java/cardreceiptmanager/controller/CardController.java
+++ src/main/java/cardreceiptmanager/controller/CardController.java
@@ -1,14 +1,21 @@
 package cardreceiptmanager.controller;
 
+import cardreceiptmanager.model.dao.CardDao;
+import cardreceiptmanager.model.entity.Card;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
 @Controller
 public class CardController {
+
+    @Autowired
+    private CardDao cardDao;
 
     @RequestMapping(value = "/cards", method = RequestMethod.GET)
     public String index(Model model) {
@@ -18,4 +25,22 @@ public class CardController {
 
         return "cards";
     }
+
+    @RequestMapping("/create")
+    @ResponseBody
+    public String create(String firstName, String lastName) {
+        String cardId = "";
+        try {
+            Card card = new Card(firstName, lastName);
+            cardDao.save(card);
+            cardId = String.valueOf(card.getId());
+        }
+        catch (Exception ex) {
+            return "Error creating the user: " + ex.toString();
+        }
+        return "User succesfully created with id = " + cardId;
+    }
+
+
+
 }
