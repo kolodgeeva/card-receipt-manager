1b9d268f164d6b769ff913cffee19ad22414a007
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-02-05 11:32:41 +0200

    Improve file uploading

--- src/main/java/cardreceiptmanager/service/CardServiceImpl.java
+++ src/main/java/cardreceiptmanager/service/CardServiceImpl.java
@@ -6,7 +6,8 @@ import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +18,8 @@ import java.net.URL;
 
 @Service
 public class CardServiceImpl implements CardService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private CardRepository cardRepository;
 
@@ -42,22 +45,24 @@ public class CardServiceImpl implements CardService {
     @Override
     public Card saveCard(Card card, MultipartFile file) throws IOException {
         if (!file.isEmpty()) {
+            // Use selected file
             byte[] bytes = file.getBytes();
             card.setFile(bytes);
         } else if (card.getId() != null) {
             Card existsCard = getCardById(card.getId());
-            card.setFile(existsCard.getFile());
+            if (existsCard != null) {
+                byte[] existsFile = existsCard.getFile();
+                if (existsFile != null) {
+                    // Use existing File for existing Card
+                    card.setFile(existsFile);
+                } else if (card.getWebLink() != null) {
+                    // Use photo from VK for existing Card
+                    setPhotoFromVK(card);
+                }
+            }
         } else if (card.getWebLink() != null) {
-            Document doc  = Jsoup.connect(card.getWebLink()).timeout(0)
-                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
-                    .referrer("http://www.google.com").get();
-            Element a = doc.select("#profile_photo_link").first();
-            Element img = a.getElementsByTag("img").get(0);
-            String src = img.absUrl("src");
-            URL url = new URL(src);
-            InputStream in = url.openStream();
-            byte[] bytes = IOUtils.toByteArray(in);
-            card.setFile(bytes);
+            // Use photo from VK for new Card
+            setPhotoFromVK(card);
         }
         return saveCard(card);
     }
@@ -74,5 +79,35 @@ public class CardServiceImpl implements CardService {
             card.setState(Card.State.BLOCKED);
             saveCard(card);
         }
+    }
+
+    private void setPhotoFromVK(Card card) {
+
+        String link = card.getWebLink();
+        if (link == null || link.isEmpty()) {
+            return;
+        }
+
+        try {
+            Document doc = Jsoup.connect(link).timeout(0)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer("http://www.google.com").get();
+            if (doc != null) {
+                Element a = doc.select("#profile_photo_link").first();
+                if (a != null) {
+                    Element img = a.getElementsByTag("img").get(0);
+                    if (img != null) {
+                        String src = img.absUrl("src");
+                        URL url = new URL(src);
+                        InputStream in = url.openStream();
+                        byte[] bytes = IOUtils.toByteArray(in);
+                        card.setFile(bytes);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.warn("File didn't uploaded ", e);
+        }
+
     }
 }
