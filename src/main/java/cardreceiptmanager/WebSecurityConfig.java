78c24eab3d9501e6e5b23205754df128ec1b5238
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-02-08 17:34:21 +0200

    Fix error with logger, Add Admin user

--- src/main/java/cardreceiptmanager/WebSecurityConfig.java
+++ src/main/java/cardreceiptmanager/WebSecurityConfig.java
@@ -30,8 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
     }
 }
