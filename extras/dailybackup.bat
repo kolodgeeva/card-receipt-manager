20612a4cb0014a4840f4b14555ecd92babba0170
Liubchenko <Elena.Liubchenko@vistajet.com>
2016-02-15 19:13:31 +0200

    fix .bat file

--- extras/dailybackup.bat
+++ extras/dailybackup.bat
@@ -1,15 +1,20 @@
 @echo off
+set BACKUPDIR=D:/Backup
+SET PGPASSWORD=djkliTR765JHlldfg
+set PGBIN="D:/PostgreSQL/9.4/bin/"
 for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
  set dow=%%i
  set month=%%j
  set day=%%k
  set year=%%l
 )
-set datestr=%month%_%day%_%year%
-echo datestr is %datestr%
 
-set BACKUP_FILE=<NameOfTheFile>_%datestr%.backup
-echo backup file name is %BACKUP_FILE%
-SET PGPASSWORD=123
-echo on
-bin\pg_dump -i -h localhost -p 5432 -U CARD_USER -F c -b -v -f %BACKUP_FILE% card_db
+for /f "tokens=1-3 delims=: " %%i in ("%time%") do (
+ set hh=%%i
+ set nn=%%j
+)
+
+
+ echo on
+ %PGBIN%pg_dump -U backup -f "D:/Backup/%year%%month%%day%%hh%.backup" card_db
+
